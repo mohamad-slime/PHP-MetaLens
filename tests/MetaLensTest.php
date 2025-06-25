050b33e252253e1aff03b40c4a746764ce245ae8
mohamad-slime <mohamadamin_dastmozdraki@outlook.com>
2025-06-25 08:46:13 +0330

    Add unit tests for MetaLens class functionality

---
+++ tests/MetaLensTest.php
@@ -0,0 +1,62 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__ . '/../MetaLens.php';
+
+class MetaLensTest extends TestCase
+{
+    private string $testImagePath;
+
+    protected function setUp(): void
+    {
+        $this->testImagePath = __DIR__ . '/images/test.jpg';
+
+        if (!file_exists($this->testImagePath)) {
+            $this->fail("Test image does not exist at: {$this->testImagePath}");
+        }
+    }
+
+    public function testAcceptsFilePath()
+    {
+        $meta = new MetaLens($this->testImagePath);
+        $this->assertEquals($this->testImagePath, $meta->getImage());
+    }
+
+    public function testAcceptsFilesArray()
+    {
+        $fakeFile = [
+            'tmp_name' => $this->testImagePath
+        ];
+
+        $meta = new MetaLens($fakeFile);
+        $this->assertEquals($this->testImagePath, $meta->getImage());
+    }
+
+    public function testRejectsInvalidInput()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new MetaLens(123); // Invalid type
+    }
+
+    public function testRejectsArrayWithoutTmpName()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new MetaLens(['name' => 'test.jpg']);
+    }
+
+    public function testThrowsExceptionWhenFileNotFound()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $meta = new MetaLens('nonexistent.jpg');
+        $meta->readMetadata(); // triggers validateImage
+    }
+
+    public function testReadMetadataReturnsArray()
+    {
+        $meta = new MetaLens($this->testImagePath);
+        $data = $meta->readMetadata();
+
+        $this->assertIsArray($data);
+    }
+}
